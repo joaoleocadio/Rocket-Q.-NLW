d76161b3c7de5e8a4f9d0a0f4b07c53235796146
joaoleocadio <joaocarvalho388@gmail.com>
2021-11-08 15:58:27 +0000

    add Projeto RocketSeat NLW

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,56 @@
+const Database = require("../db/config")
+
+module.exports = {
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password;
+    let roomId;
+    let isRoom = true;
+
+    while (isRoom) {
+      /* Gerar o nº da Sala */
+      for (var i = 0; i < 6; i++) {
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+          roomId += Math.floor(Math.random() * 10).toString()
+      }
+
+      /* Verificar se o nº da sala já existe na BD*/
+      const roomsExistsIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistsIds.some(roomsExistsIds => roomsExistsIds === roomId)
+
+      if (!isRoom) {
+        /* Inserir a sala no Base de Dados */
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
+    }
+
+    await db.close()
+
+    res.redirect(`/room/${roomId}`);
+  },
+
+  async open(req, res) {
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    let isNoQuestion;
+
+    if (questions.length == 0 && questionsRead.length == 0) {
+      isNoQuestion = true;
+    }
+    res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestion: isNoQuestion })
+  },
+
+  enter(req, res) {
+    const roomId = req.body.roomId;
+
+    res.redirect(`/room/${roomId}`);
+  }
+}
